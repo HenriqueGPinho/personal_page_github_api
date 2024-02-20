a014e31e27606612cd2f3300bc1b8917115c88ba
Henrique Pinho <henriquegravino@outlook.com>
2024-02-20 17:55:49 -0300

    add: fetch github

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
+  const avatar = document.querySelector('#profile-avatar');
+  const name = document.querySelector('#profile-name');
+  const username = document.querySelector('#profile-username');
+  const numberRepos = document.querySelector('#profile-repos');
+  const numberFollowers = document.querySelector('#profile-followers');
+  const numberFollowing = document.querySelector('#profile-following');
+  const link = document.querySelector('#profile-link')
+
+  fetch('https://api.github.com/users/HenriqueGPinho')
+    .then(function (r) {
+      return r.json();
+    })
+    .then(function (json) {
+      avatar.src = json.avatar_url;
+      name.innerText = json.name;
+      username.innerText = json.login;
+      numberRepos.innerText = json.public_repos;
+      numberFollowers.innerText = json.followers;
+      numberFollowing.innerText = json.following;
+      link.href = json.html_url;
+    });
 });
